b90302f376a85c60c843f58edd5afa4948f9a260
lenariusa <55251136+lenariusa@users.noreply.github.com>
2025-04-30 11:57:29 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,12 +1,12 @@
 import streamlit as st
 from supabase import create_client, Client
 
-# Вставь сюда свои данные
+# Получение секретов из streamlit
 url = st.secrets["SUPABASE_URL"]
 key = st.secrets["SUPABASE_KEY"]
 
 # Создание клиента Supabase
-supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
+supabase: Client = create_client(url, key)
 
 # Получение данных из таблицы "repairs"
 response = supabase.table("repairs").select("*").execute()
